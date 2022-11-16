410af145d8ca998246700fc60355ac4fd4b24531
enmourn <enmourn@gmail.com>
2022-11-16 21:40:14 +0300

    rehershals calendar refactor

--- src/routes/admin.jsx
+++ src/routes/admin.jsx
@@ -35,39 +35,20 @@ export async function loader({ params }) {
 export default function Admin() {
   const band = useLoaderData()
   const rehershals = band.rehershals ? rehershalsToTree(band.rehershals) : null
-
   const dateNow = new Date
-  const dateFirstRehershal = (() => {
-    if (band.rehershals) {
-      let dateStr = Object.keys(band.rehershals)[0]
-      let [year, month, day] = dateStr.split('-').map(str => parseInt(str))
-      return new Date(year, month - 1, day)
-    } else {
-      return dateNow
-    }
-  })()
-  const [currentCalendar, setCurrentCalendar] = useState({
-    year: dateNow.getFullYear(),
-    month: dateNow.getMonth()
-  })
-  const getCalendar = num => {
-    let date = new Date(currentCalendar.year, currentCalendar.month + num)
-    return {year: date.getFullYear(), month: date.getMonth()}
-  }
-  const getDates = () => {
-    let year = currentCalendar.year
-    let month = currentCalendar.month + 1
+  const [dateCurrent, setDateCurrent] = useState(dateNow)
+  const calendarGetDates = () => {
+    let year = dateCurrent.getFullYear()
+    let month = dateCurrent.getMonth() + 1
     return rehershals && rehershals[year] && rehershals[year][month] || {}
   }
   const calendarPrev = () => {
-    return new Date(currentCalendar.year, currentCalendar.month) > dateFirstRehershal ?
-      () => setCurrentCalendar(getCalendar(-1)) :
-      false
+    let datePrev = new Date(dateCurrent.getFullYear(), dateCurrent.getMonth() - 1)
+    return () => setDateCurrent(datePrev)
   }
   const calendarNext = () => {
-    return new Date(currentCalendar.year, currentCalendar.month + 1) < dateNow ?
-      () => setCurrentCalendar(getCalendar(+1)) :
-      false
+    let dateNext = new Date(dateCurrent.getFullYear(), dateCurrent.getMonth() + 1)
+    return dateNext <= dateNow ? () => setDateCurrent(dateNext) : false
   }
   const calendarClick = (day) => {
     console.log(day)
@@ -75,9 +56,9 @@ export default function Admin() {
   return (
     <Grid gap={6}>
       <Calendar
-        year={currentCalendar.year}
-        month={currentCalendar.month + 1}
-        dates={getDates()}
+        year={dateCurrent.getFullYear()}
+        month={dateCurrent.getMonth() + 1}
+        dates={calendarGetDates()}
         prev={calendarPrev()}
         next={calendarNext()}
         click={calendarClick}
