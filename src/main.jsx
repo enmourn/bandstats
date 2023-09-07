c5a288eb2b076dba2da7a8cc758424c3b57f1e0a
enmourn <enmourn@gmail.com>
2023-09-07 14:06:36 +0300

    remote replace

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react"
+import ReactDOM from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { initializeApp } from "firebase/app"
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
-import { ConfirmContextProvider } from './components/ConfirmContextProvider'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+import { ConfirmContextProvider } from "./components/ConfirmContextProvider"
 
-import Root from './routes/root'
-import { Index, indexLoader, indexAction } from './routes/index'
-import Auth, {
-  loader as authLoader,
-  action as authAction
-} from './routes/auth'
-import Registration,  {
-  loader as registrationLoader,
-  action as registrationAction
-} from './routes/registration'
-import Band, {
-  loader as bandLoader
-} from './routes/band'
-import Admin, {
-  loader as adminLoader,
-  action as adminAction
-} from './routes/admin'
-import Error from './routes/error.jsx'
+import Root from "./routes/root"
+import { Index, indexLoader, indexAction } from "./routes/index"
+import Auth, { loader as authLoader, action as authAction } from "./routes/auth"
+import Registration, { loader as registrationLoader, action as registrationAction } from "./routes/registration"
+import Band, { loader as bandLoader } from "./routes/band"
+import Admin, { loader as adminLoader, action as adminAction } from "./routes/admin"
+import Error from "./routes/error.jsx"
 
 const firebaseConfig = {
   apiKey: "AIzaSyC2AO9CBep5oe7pz-eJYLaX-iFXjE9aWZ0",
@@ -31,74 +20,79 @@ const firebaseConfig = {
   storageBucket: "bandstats-d5330.appspot.com",
   messagingSenderId: "240207236904",
   appId: "1:240207236904:web:231a711ca3b0afa0028a1d",
-  measurementId: "G-E69WTB8DNE"
-};
-const app = initializeApp(firebaseConfig);
+  measurementId: "G-E69WTB8DNE",
+}
+const app = initializeApp(firebaseConfig)
 
 const theme = extendTheme({
   components: {
     Checkbox: {
       variants: {
-        "white": {
+        white: {
           control: {
-            bg: "white"
-          }
-        }
-      }
+            bg: "white",
+          },
+        },
+      },
     },
     Link: {
       baseStyle: {
-        textDecoration: 'underline',
-        color: 'teal'
-      }
-    }
-  }
+        textDecoration: "underline",
+        color: "teal",
+      },
+    },
+  },
 })
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          errorElement: <Error />,
+          children: [
+            {
+              index: true,
+              element: <Index />,
+              loader: indexLoader,
+              action: indexAction,
+            },
+            {
+              path: "auth",
+              element: <Auth />,
+              loader: authLoader,
+              action: authAction,
+            },
+            {
+              path: "registration",
+              element: <Registration />,
+              loader: registrationLoader,
+              action: registrationAction,
+            },
+            {
+              path: ":band",
+              element: <Band />,
+              loader: bandLoader,
+            },
+            {
+              path: ":band/admin",
+              element: <Admin />,
+              loader: adminLoader,
+              action: adminAction,
+            },
+          ],
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <Root />,
-    children: [
-      {
-        errorElement: <Error />,
-        children: [
-          {
-            index: true,
-            element: <Index />,
-            loader: indexLoader,
-            action: indexAction
-          },
-          {
-            path: 'auth',
-            element: <Auth />,
-            loader: authLoader,
-            action: authAction
-          },
-          {
-            path: 'registration',
-            element: <Registration />,
-            loader: registrationLoader,
-            action: registrationAction
-          },
-          {
-            path: ':band',
-            element: <Band />,
-            loader: bandLoader
-          },
-          {
-            path: ':band/admin',
-            element: <Admin />,
-            loader: adminLoader,
-            action: adminAction
-          }
-        ]
-      }
-    ]
-  },
-])
+    basename: "/bandstats",
+  }
+)
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ConfirmContextProvider>
